e520aca971aa92c9b2fb958a73a4f724bb0d92ae
oddjobsman <oddjobsman@oddjobsman.me>
2014-05-15 00:40:57 +0530

    added program to demonstrate use of blocks to assign lvalues and unit type

---
+++ blockexpression.rs
@@ -0,0 +1,24 @@
+// blockexpression.rs
+fn main() {
+	// all type annotations are superfluous
+	let x: int = 5;
+	
+	let y: int = {
+		// assignment statements are allowed inside blocks
+		let x_squared = x * x;
+		let x_cube = x_squared * x;
+		
+		// this expression will be assigned to y
+		x_cube + x_squared + x
+	};
+	
+	let z: () = {
+		// the semicolon suppresses this expression,
+		// and `()` is assigned to z
+		2 * x;
+	};
+	
+	println!("x is {}", x);
+	println!("y is {}", y);
+	println!("z is {}", z);
+}
