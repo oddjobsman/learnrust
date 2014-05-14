c0c284c04dbb2340828c2418a2f6f120e4e06650
oddjobsman <oddjobsman@oddjobsman.me>
2014-05-15 00:41:17 +0530

    added program to demonstrate use of if-else statements

---
+++ if-else.rs
@@ -0,0 +1,12 @@
+// if-else.rs
+fn main() {
+	let n = 5;
+	
+	if n < 0 {
+		println!("{} is negative", n);
+	} else if n > 0 {
+		println!("{} is positive", n);
+	} else {
+		println!("{} is zero!", n);
+	}
+}
