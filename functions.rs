49aa5bf5d84c6505b50175c15e94a34bfb12fc35
oddjobsman <oddjobsman@oddjobsman.me>
2014-05-15 13:10:43 +0530

    added program to demonstrate functions

---
+++ functions.rs
@@ -0,0 +1,31 @@
+// functions.rs
+
+// function that returns a bool
+fn is_divisible_by(lhs: uint, rhs: uint) -> bool {
+	// corner case, early return
+	if rhs == 0 {
+		return false;
+	}
+
+	// this is an expression, the 'return' keyword is not necessary here
+	lhs % rhs == 0
+}
+
+// function does not return a value
+fn fizzbuzz(n: uint) {
+	if is_divisible_by(n, 15) {
+		println!("fizzbuzz");
+	} else if is_divisible_by(n, 3) {
+		println!("fizz");
+	} else if is_divisible_by(n, 5) {
+		println!("buzz");
+	} else {
+		println!("{}", n);
+	}
+}
+
+fn main() {
+	for n in range(1u, 101) {
+		fizzbuzz(n);
+	}
+}
